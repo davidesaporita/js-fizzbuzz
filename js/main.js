81a881c7ba086da22574ce81d8818d799cc3e232
Davide Saporita <davide.saporita@gmail.com>
2020-04-01 13:16:19 +0200

    JS | Basic mode completed

--- js/main.js
+++ js/main.js
@@ -5,3 +5,27 @@
  * per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
  */
 
+// Inizializzazione variabile per stampa
+var boom = '';
+
+// Ciclo per 100 numeri
+for(i=1;i<=100;i++) {
+    // Se il numero da stampare è un multiplo di 3, aggiungi 'Fizz' alla stringa boom 
+    if (i % 3 == 0) {
+        boom += 'Fizz';
+    }
+    // Se il numero da stampare è un multiplo di 3, aggiungi 'Buzz' alla stringa boom 
+    if (i % 5 == 0) {
+        boom += 'Buzz';
+    }
+
+    // Se la variabile boom è vuota, allora non è un multiplo di 3 o 5, altrimenti stampa Fizz / Buzz / o una combinazione di entrambe
+    if (boom == '') {
+        console.log(i);
+    } else {
+        console.log(boom);
+    }  
+
+    // Re-inizializzazione variabile
+    boom = '';
+}
